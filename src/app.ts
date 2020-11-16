e9ab793932f057e45c5d2d583c6458f86860a8f3
Alberto Vélaz <albertovelaz.dev@gmail.com>
2020-11-16 06:55:31 +0100

    Add rate limit to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import RateLimit from 'express-rate-limit'
 import Helmet from 'helmet'
 
-import { APIRoute } from '../src/routes/api'
-import { ImageRoute } from '../src/routes/image'
+import { APIRoute } from './routes/api'
+import { ImageRoute } from './routes/image'
+import { constants } from './utils/constants'
+
+const limiter = RateLimit({
+  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: constants.MINUTES_15
+});
 
 class App {
   public app: express.Application
@@ -14,6 +21,7 @@ class App {
     this.app = express() 
     this.app.use(bodyParser.json())
     this.app.use(Helmet())
+    this.app.use(limiter)
     this.imageRoutes.routes(this.app)
     this.apiRoutes.routes(this.app)
   }
