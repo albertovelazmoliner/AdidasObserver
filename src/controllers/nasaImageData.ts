095189abf32daf2e54a3e2aa1ee842b4d4987c75
Alberto Vélaz <albertovelaz.dev@gmail.com>
2020-11-16 20:31:37 +0100

    Add logs to ImageDataController

--- src/controllers/nasaImageData.ts
+++ src/controllers/nasaImageData.ts
@@ -4,9 +4,13 @@ import NasaImageData from '../model/nasaImageData'
 import gmClient from '../provider/gmClient'
 import { NasaUrlComposer } from '../provider/nasaUrlComposer'
 import { validationResult } from 'express-validator'
+import { AdidasObserverAPILogger } from '../utils/logger'
 
 export class ImageDataController {    
-    public static  async getNasaImage(req: Request, res: Response, _next: NextFunction) {        
+    public static  async getNasaImage(req: Request, res: Response, _next: NextFunction) {
+        
+        AdidasObserverAPILogger.logger.info(`[GET] [/image] [start] ${JSON.stringify(req.query)}`)
+        
         let geocodeResult: GeocodeResponse
         let nasaImage: NasaImageData
 
@@ -25,12 +29,16 @@ export class ImageDataController {
                 lng: geocodeResult.data.results[0].geometry.location.lng,
                 formattedAddress: geocodeResult.data.results[0].formatted_address
             }
-            // tslint:disable-next-line:no-console
-            console.log(nasaParams)
+
+            AdidasObserverAPILogger.logger.info(`[GET] [/image] [nasaParams] ${JSON.stringify(nasaParams)}`)
+
             nasaImage = NasaUrlComposer.urlGenerator(nasaParams);
         } catch (err) {
+            
             if (Array.isArray(err.errors) && err.errors[0]) {
+                AdidasObserverAPILogger.logger.error(`[GET] [/image] ${JSON.stringify(err)}`)
                 const { param, msg } = err.errors[0]
+                AdidasObserverAPILogger.logger.info(`[GET] [/image] [finish/paramsError]}`)
                 if (param === 'api_key') {
                     return res.status(401).send({ errorMessage: msg})    
                 }
@@ -39,9 +47,11 @@ export class ImageDataController {
                 }
             }
             
+            AdidasObserverAPILogger.logger.error(`[GET] [/image] { error: ${err.message} }`)
+            AdidasObserverAPILogger.logger.info(`[GET] [/image] [finish/geoCodeError]}`)
             return res.status(404).send({ error: err.message })
         }
-
+        AdidasObserverAPILogger.logger.info(`[GET] [/image] [finish] ${JSON.stringify(nasaImage)}`)
         return res.status(200).send(nasaImage)
     }
 }
